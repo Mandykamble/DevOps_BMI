00546a459dc7c6ff0d9921ba8ccbf0613fe10442
sk22082001 <165132458+sk22082001@users.noreply.github.com>
2024-03-27 12:48:44 +0530

    Update bmi-calculator.js
    changes made before lunch

--- bmi-calculator.js
+++ bmi-calculator.js
@@ -16,7 +16,7 @@ app.post("/calculate", (req, res) => {
 function calculateBMI(weight, height) {
   return weight / (height * height);
 }
-
+// comment made by subham kumar
 const port = process.env.PORT || 8000;
 app.listen(port, () =>
   console.log(
